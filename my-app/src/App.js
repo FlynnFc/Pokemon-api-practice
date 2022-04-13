b523130dad60806c87db59c24541374bd744a92e
Flynn FC <mastare.flynn@gmail.com>
2022-04-13 17:04:53 +0100

    State management for input

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,11 +4,15 @@ import Pokemon from "./components/Pokemon";
 import Selector from "./components/Selector";
 
 function App() {
+  const totalGetter = (inputVal) => {
+    let inputFromForm = inputVal;
+  };
+
   return (
     <div className="font-bold text-5xl text-white text-center">
       <Banner></Banner>
-      <Selector></Selector>
-      <Pokemon></Pokemon>
+      <Selector totalGetter={totalGetter}></Selector>
+      <Pokemon inputVal={inputVal}></Pokemon>
     </div>
   );
 }
--- my-app/src/components/Selector.js
+++ my-app/src/components/Selector.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Selector = (props) => {
+  const numberInput = document.querySelector("#numberSelector");
+  let numberVal = numberInput.value;
+  const [total, setTotal] = useState(0);
+  props.totalGetter(total);
+
   return (
     <>
       <input
+        onInput={() => setTotal({ numberVal })}
         type="number"
         id="numberSelector"
         min="1"
